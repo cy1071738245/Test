d829c5190fb7f4105199893d7cf1a457f219bb92
chenyu <chenyu@zhiweidata.com>
2020-03-25 18:02:10 +0800

    sql上传

---
+++ src/main/resources/poetry.sql
@@ -0,0 +1,76 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 渠道标签线上mysql
+ Source Server Type    : MySQL
+ Source Server Version : 50725
+ Source Host           : 115.236.59.91:3306
+ Source Schema         : channeltag
+
+ Target Server Type    : MySQL
+ Target Server Version : 50725
+ File Encoding         : 65001
+
+ Date: 25/03/2020 18:00:50
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for author
+-- ----------------------------
+DROP TABLE IF EXISTS `author`;
+CREATE TABLE `author`  (
+  `author_id` int(11) NOT NULL AUTO_INCREMENT,
+  `author_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '' COMMENT '诗人名',
+  `sex` enum('男','女') CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '性别',
+  `dynasty` enum('唐朝','宋朝') CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '朝代',
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT '诗人简介',
+  `update_time` timestamp(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  PRIMARY KEY (`author_id`) USING BTREE,
+  UNIQUE INDEX `author_name_dynasty_index`(`author_name`, `dynasty`) USING BTREE COMMENT '诗人名唯一索引'
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of author
+-- ----------------------------
+INSERT INTO `author` VALUES (1, '李白', '男', '唐朝', NULL, '2020-03-25 17:10:48');
+INSERT INTO `author` VALUES (2, '杜甫', '男', '唐朝', NULL, '2020-03-25 17:10:51');
+INSERT INTO `author` VALUES (3, '白居易', '男', '唐朝', NULL, '2020-03-25 17:10:56');
+
+-- ----------------------------
+-- Table structure for poetry
+-- ----------------------------
+DROP TABLE IF EXISTS `poetry`;
+CREATE TABLE `poetry`  (
+  `poetry_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '诗词名',
+  `author_id` int(11) NOT NULL COMMENT '作者id',
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '诗词内容',
+  `image_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `update_time` timestamp(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`poetry_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of poetry
+-- ----------------------------
+INSERT INTO `poetry` VALUES (1, '静夜思', 1, '床前明月光，疑是地上霜。\r\n举头望明月，低头思故乡。', NULL, '2020-03-07 21:01:40');
+INSERT INTO `poetry` VALUES (2, '赠汪伦', 1, '李白乘舟将欲行，忽闻岸上踏歌声。\r\n桃花潭水深千尺，不及汪伦送我情。', NULL, '2020-03-07 21:02:03');
+INSERT INTO `poetry` VALUES (3, '赋得古原草送别', 3, '离离原上草，一岁一枯荣。\r\n野火烧不尽，春风吹又生。\r\n远芳侵古道，晴翠接荒城。\r\n又送王孙去，萋萋满别情。', NULL, '2020-03-07 21:04:14');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '用户名',
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '密码',
+  `real_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '真实名字',
+  `update_time` timestamp(0) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
